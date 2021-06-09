1508c28d7a471d50647d8f62d37150f826bee1a4
wafash08 <saefulhaqwafa@gmail.com>
2021-06-10 00:32:07 +0700

    Inisialisasi awal

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".nav .hamburger");
+const mobile_menu = document.querySelector(".nav-menu ul");
+const menu_item = document.querySelectorAll(".nav-menu ul li a");
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
